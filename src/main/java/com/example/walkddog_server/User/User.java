0a28744bf7f614ab0343b1e6497cff62cd664b8d
Or <or.othnay@gmail.com>
2022-04-24 18:56:41 +0300

    getUserByUsername returns list of user dogs

--- src/main/java/com/example/walkddog_server/User/User.java
+++ src/main/java/com/example/walkddog_server/User/User.java
@@ -1,18 +1,20 @@
 package com.example.walkddog_server.User;
 
+import com.example.walkddog_server.Dog.Dog;
 import lombok.Data;
+
+import java.util.List;
 
 public @Data
 class User {
     private String username;
-
     //TODO add hashing/salting
     private String password;
-
-
     private String email;
     private String first_name;
     private String last_name;
+
+    private List<Dog> dogs;
 
 
     public User(String username, String password, String email, String first_name, String last_name) {
@@ -21,7 +23,12 @@ class User {
         this.email = email;
         this.first_name = first_name;
         this.last_name = last_name;
+        this.dogs = null;
+    }
 
+    public User whereDogs(List<Dog> dogs) {
+        this.dogs = dogs;
+        return this;
     }
 
 }
