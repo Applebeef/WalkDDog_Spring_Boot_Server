e3cbfc19afc2c9742877294a8a73831ae36d4d5e
Applebeef <58487936+Applebeef@users.noreply.github.com>
2022-05-12 23:03:50 +0300

    Added a bit of documentation

--- src/main/java/com/example/walkddog_server/Config/Constants.java
+++ src/main/java/com/example/walkddog_server/Config/Constants.java
@@ -3,7 +3,7 @@ package com.example.walkddog_server.Config;
 import lombok.Data;
 
 public @Data
-class Constants {
+class Constants { //Paths that are used in several locations across the server.
     public static final String uploadPath = "C:/Spring Boot Server/DogPics/";
     public static final String fileSystemPath = "file:///" + uploadPath;
     public static final String staticPath = "classpath:/static/";
